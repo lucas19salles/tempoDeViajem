30a3c924fdf7557d20b5c104e0c969ed7fc1133b
lucas19salles <lucassalles19@hotmail.com>
2022-04-23 13:44:35 -0300

    Adicionado anos

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let nameTraveler = prompt("Qual é o seu nome viajante do tempo? ");
 let departureDateEntry = prompt(
   "Digite a data de partida (formato DD/MM/YYYY)"
 );
@@ -14,19 +15,23 @@ let chosenOption = prompt(
 
 if (chosenOption == "1") {
   let secondsOfDeparture = Math.round(dateDiff / 1000);
-  alert(`Tempo de vôo ${secondsOfDeparture} segundos`);
+  alert(
+    `Tempo de vôo ${secondsOfDeparture} segundos, Viajante ${nameTraveler}`
+  );
 } else if (chosenOption == "2") {
   let minutsOfDeparture = Math.round(dateDiff / 1000 / 60);
-  alert(`Tempo de vôo ${minutsOfDeparture} minutos`);
+  alert(`Tempo de vôo ${minutsOfDeparture} minutos, Viajante ${nameTraveler}`);
 } else if (chosenOption == "3") {
   let hoursOfDeparture = Math.round(dateDiff / 1000 / 3600);
-  alert(`Tempo de vôo ${hoursOfDeparture} horas`);
+  alert(`Tempo de vôo ${hoursOfDeparture} horas, Viajante ${nameTraveler}`);
 } else if (chosenOption == "4") {
   let daysOfDeparture = Math.round(dateDiff / 1000 / 3600 / 24);
-  alert(`Tempo de vôo ${daysOfDeparture} dias`);
+  alert(`Tempo de vôo ${daysOfDeparture} dias, Viajante ${nameTraveler}`);
 } else if (chosenOption == "5") {
   let yearsOfDeparture = Math.round(dateDiff / 1000 / 3600 / 24 / 365);
-  alert(`Faz ${yearsOfDeparture} anos que você está voando.`);
+  alert(
+    `Faz ${yearsOfDeparture} anos que você está voando, Viajante ${nameTraveler}`
+  );
 } else {
   alert("Opção Invalido");
 }
